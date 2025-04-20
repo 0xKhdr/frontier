caf532bf1072f5f0d9a0d2d090113498f470518b
0xKhdr <0xkhdr@gmail.com>
2025-04-20 14:07:21 +0200

    Register starter commands

--- src/Providers/StarterServiceProvider.php
+++ src/Providers/StarterServiceProvider.php
@@ -2,11 +2,19 @@
 
 namespace Frontier\Providers;
 
+use Frontier\Console\Commands\InstallCommand;
 use Illuminate\Support\ServiceProvider;
 
 class StarterServiceProvider extends ServiceProvider
 {
+    protected array $commands = [
+        InstallCommand::class,
+    ];
+
 	public function register(): void {}
 
-	public function boot(): void {}
+	public function boot(): void
+    {
+        $this->commands($this->commands);
+    }
 }
